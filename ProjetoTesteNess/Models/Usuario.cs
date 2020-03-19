aef1bc6c858c4a9d57c0c3feef423e8042f82ad7
caco85 <renato_caco85@hotmail>
2020-03-19 13:48:20 -0300

    Melhorando um pouco parte routa e front end

--- ProjetoTesteNess/Models/Usuario.cs
+++ ProjetoTesteNess/Models/Usuario.cs
@@ -9,11 +9,13 @@ namespace ProjetoTesteNess.Models
 {
     public class Usuario
     {
-        public int userID{get;set;}
-
+        public int userID { get; set; }
+        [Display(Name = "Nome")]
         public string nome { get; set; }
-  //      [RegularExpression(@"[0-9]{3}.\[0-9]{3}.\[0-9]{3}.\[0-9]{2}-\", ErrorMessage = "O cpf tem que ser 000.000.000-19")]
+        //      [RegularExpression(@"[0-9]{3}.\[0-9]{3}.\[0-9]{3}.\[0-9]{2}-\", ErrorMessage = "O cpf tem que ser 000.000.000-19")]
+        [Display(Name = "CPF")]
         public string cpf { get; set; }
+        [Display(Name = "Telefone")]
         public string telefone { get; set; }
 
     }
